ba7ebcd91e1960490e64574270512b68150ea284
Jose Britto <jchdb7@gmail.com>
2023-12-27 23:21:52 -0300

    feat: implementado estrutura de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { NoticeCardComponent } from './components/notice-card/notice-card.component';
+import { NewsletterCardComponent } from './components/newsletter-card/newsletter-card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    NoticeCardComponent,
+    NewsletterCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
